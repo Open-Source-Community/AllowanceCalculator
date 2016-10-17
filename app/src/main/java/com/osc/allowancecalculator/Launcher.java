3d3607ac7df233799742d55b602aa64ddfeef6b1
devar1 <ahmed.reda.mohamed@outlook.com>
2016-10-17 18:28:43 +0200

    fixed issue :  not updating data when coming from background

--- app/src/main/java/com/osc/allowancecalculator/Launcher.java
+++ app/src/main/java/com/osc/allowancecalculator/Launcher.java
@@ -26,38 +26,44 @@ public class Launcher extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
            @Override
            public void run() {
-               Calendar Date = Calendar.getInstance();
-               final int year=Date.get(Calendar.YEAR);
-               final int month=Date.get(Calendar.MONTH);
-               final int day=Date.get(Calendar.DAY_OF_MONTH);
-               SharedPreferences date=getSharedPreferences("date",MODE_PRIVATE);
-               int lastyear=date.getInt("year",0);
-               int lastmonth=date.getInt("month",0);
-               int lastday=date.getInt("day",0);
-               if(lastday!=0) {
-                   Calendar today = Calendar.getInstance();
-                   today.set(year, month, day);
-                   Calendar endDate = Calendar.getInstance();
-                   endDate.set(lastyear, lastmonth, lastday);
-                   long difference = today.getTimeInMillis() - endDate.getTimeInMillis();
-                   long diffenceInDays = difference / (24 * 60 * 60 * 1000);
-                   SharedPreferences moneyData = getSharedPreferences("moneydata", MODE_PRIVATE);
-                   long numberOfDays = moneyData.getLong("numberofdays", -1);
-                   long newNumberOfDays=numberOfDays-diffenceInDays;
-                   SharedPreferences.Editor moneydataEditor=moneyData.edit();
-                   moneydataEditor.putLong("numberofdays",newNumberOfDays);
-                   moneydataEditor.commit();
-                   SharedPreferences.Editor dateEditor=date.edit();
-                   dateEditor.putInt("year",year);
-                   dateEditor.putInt("month",month);
-                   dateEditor.putInt("day",day);
-                   dateEditor.commit();
-               }
+
                Intent homeIntent=new Intent(Launcher.this,Home.class);
                startActivity(homeIntent);
                finish();
            }
        },3000);
 
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Calendar Date = Calendar.getInstance();
+        final int year=Date.get(Calendar.YEAR);
+        final int month=Date.get(Calendar.MONTH);
+        final int day=Date.get(Calendar.DAY_OF_MONTH);
+        SharedPreferences date=getSharedPreferences("date",MODE_PRIVATE);
+        int lastyear=date.getInt("year",0);
+        int lastmonth=date.getInt("month",0);
+        int lastday=date.getInt("day",0);
+        if(lastday!=0) {
+            Calendar today = Calendar.getInstance();
+            today.set(year, month, day);
+            Calendar endDate = Calendar.getInstance();
+            endDate.set(lastyear, lastmonth, lastday);
+            long difference = today.getTimeInMillis() - endDate.getTimeInMillis();
+            long diffenceInDays = difference / (24 * 60 * 60 * 1000);
+            SharedPreferences moneyData = getSharedPreferences("moneydata", MODE_PRIVATE);
+            long numberOfDays = moneyData.getLong("numberofdays", -1);
+            long newNumberOfDays=numberOfDays-diffenceInDays;
+            SharedPreferences.Editor moneydataEditor=moneyData.edit();
+            moneydataEditor.putLong("numberofdays",newNumberOfDays);
+            moneydataEditor.commit();
+            SharedPreferences.Editor dateEditor=date.edit();
+            dateEditor.putInt("year",year);
+            dateEditor.putInt("month",month);
+            dateEditor.putInt("day",day);
+            dateEditor.commit();
+        }
     }
 }
